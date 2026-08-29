c37705fc7515e57de1b74a773f1fad0af5130aec
keyur1277 <keyur1277@gmail.com>
2026-08-30 00:33:05 +0530

    Update baseUrl in quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "keyur1277.github.io/SpeechOrigins/",
+    baseUrl: "keyur1277.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
